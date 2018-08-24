a927afe56ad8a03cbdf2a62b5d58528e8a1b795f
Unknown <llogan382@gmail.com>
2018-08-24 08:54:18 -0400

    get elements by CLASS name

--- scripts.js
+++ scripts.js
@@ -3,3 +3,9 @@ const myList = document.getElementsByTagName('li');
 for (let i = 0; i < myList.length; i ++ ) {
     myList[i].style.color = 'purple';
 }
+
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i ++ ) {
+    errorNotPurple[i].style.color = 'red';
+}
