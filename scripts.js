99e64fcdea0081b2d49a1e9d4390c10dd79c42f5
Unknown <llogan382@gmail.com>
2018-08-24 08:26:11 -0400

    get element by Tag Name

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
-const myHeading = document.getElementById('myHeading');
+const myHeading = document.getElementsByTagName('h1')[0];
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
