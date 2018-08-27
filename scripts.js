e674706b45f1f1552b85928716f6da4205e5ca42
Unknown <llogan382@gmail.com>
2018-08-27 12:27:34 -0400

    Add and remove elements from the list

--- scripts.js
+++ scripts.js
@@ -5,6 +5,8 @@ const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('Button.addItemButton');
+const removeItemButton = document.querySelector('Button.removeItemButton');
+
 
 
 toggleList.addEventListener('click', () => { //when the toggle is clicked:
@@ -19,12 +21,20 @@ toggleList.addEventListener('click', () => { //when the toggle is clicked:
 
 descriptionButton.addEventListener('click', () => {
     descriptionP.innerHTML = descriptionInput.value + ':';
+    descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];//this returns a COLLECTION; since there is only 1 item, we can access it with [0]
     let li = document.createElement('li');
     li.textContent = addItemInput.value;
-    ul.appendChild(li);
+    ul.appendChild(li); //take the newly creates var and add it to the "li"
+    addItemInput.value = '';
+});
+
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];//this returns a COLLECTION; since there is only 1 item, we can access it with [0]
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li); //take the newly creates var and add it to the "li"
 });
 
