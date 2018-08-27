fd9f3a3f47279cdf41ef86c5c5a755141f36e97e
Unknown <llogan382@gmail.com>
2018-08-27 09:33:35 -0400

    Add script from course files

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
-const myList = document.getElementsByTagName('li');
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const input = document.querySelector('input.description');
+const p = document.querySelector('p.description');
+const button = document.querySelector('button.description');
 
-for (let i = 0; i < myList.length; i ++ ) {
-    myList[i].style.color = 'purple';
-}
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
+    toggleList.textContent = 'Show list';                        
+    listDiv.style.display = 'none';
+  }                         
+});
 
-const errorNotPurple = document.querySelectorAll('.error-not-purple');
+button.addEventListener('click', () => {
+  p.innerHTML = input.value + ':';
+});
 
-for (let i = 0; i < errorNotPurple.length; i ++ ) {
-    errorNotPurple[i].style.color = 'red';
-}
-
-const evens = document.querySelectorAll('li:nth-child(even)');
-
-for (let i = 0; i < evens.length; i ++ ) {
-    evens[i].style.backgroundColor = 'lightgray';
-}
