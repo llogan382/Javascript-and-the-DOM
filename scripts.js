46bf752f225e07f49604042275d3b8f4d46dcc36
Unknown <llogan382@gmail.com>
2018-08-27 11:30:48 -0400

    Rnames objects to be more specific

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 const toggleList = document.getElementById('toggleList');
 const listDiv = document.querySelector('.list');
-const input = document.querySelector('input.description');
-const p = document.querySelector('p.description');
-const button = document.querySelector('button.description');
+const descriptionInput = document.querySelector('input.description');
+const descriptionP = document.querySelector('p.description');
+const descriptionButton = document.querySelector('button.description');
+const addItemInput = document.querySelector('input.addItemInput');
+const addItemButton = document.querySelector('Button.addItemButton');
+
 
 toggleList.addEventListener('click', () => { //when the toggle is clicked:
   if (listDiv.style.display == 'none') { //if the Div with class list is set to NONE:
@@ -14,7 +17,12 @@ toggleList.addEventListener('click', () => { //when the toggle is clicked:
   }                         
 });
 
-button.addEventListener('click', () => {
-  p.innerHTML = input.value + ':';
+descriptionButton.addEventListener('click', () => {
+    descriptionP.innerHTML = descriptionInput.value + ':';
+});
+
+addItemButton.addEventListener('click', () => {
+    let li = document.createElement('li');
+    li.textContent = addItemInput.value;
 });
 
