23298b292e9c42afe157f865434151f7a55f229e
Unknown <llogan382@gmail.com>
2018-09-03 06:45:00 -0400

    Adds course files

--- scripts.js
+++ scripts.js
@@ -4,38 +4,45 @@ const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
 const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
-const addItemButton = document.querySelector('Button.addItemButton');
+const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
-const listItems = document.getElementsByTagName('li');
 
-for (let i=0; i< listItems.length; i += 1 ) {
-    //functionname(created above).function('what to listen for', callback function)
-    listItems[i].addEventListener('mouseover', () => {
-        listItems[i].textContent = listItems[i].textContent.toUpperCase();
-    })
-    listItems[i].addEventListener('mouseout', () => {
-        listItems[i].textContent = listItems[i].textContent.toLocaleLowerCase();
-    })
-}
+// listDiv.addEventListener('mouseover', () => {
+//   listItems[i].textContent = listItems[i].textContent.toUpperCase();
+// });
+// listDiv.addEventListener('mouseout', () => {
+//   listItems[i].textContent = listItems[i].textContent.toLowerCase();
+// });
 
-toggleList.addEventListener('click', () => { //when the toggle is clicked:
-  if (listDiv.style.display == 'none') { //if the Div with class list is set to NONE:
-    toggleList.textContent = 'Hide list'; //changes button text depending on status
-    listDiv.style.display = 'block'; //change the display style to "block"
-  } else {  // else change the display to "none"
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display == 'none') {
+    toggleList.textContent = 'Hide list';
+    listDiv.style.display = 'block';
+  } else {
     toggleList.textContent = 'Show list';                        
     listDiv.style.display = 'none';
   }                         
 });
 
 descriptionButton.addEventListener('click', () => {
-    descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionP.innerHTML = descriptionInput.value + ':';
+  descriptionInput.value = '';
 });
 
 addItemButton.addEventListener('click', () => {
-    let ul = document.getElementsByTagName('ul')[0];//this returns a COLLECTION; since there is only 1 item, we can access it with [0]
-    let li = document.createElement('li');
-    li.textContent = addItemInput.value;
-    ul.appendChild(li);
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.createElement('li');
+  li.textContent = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = '';
 });
-
+  
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+});
+  
+  
+  
+  
