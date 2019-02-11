0537395746abab971d9254e52a1a24005f72f3aa
Luke Logan <llogan382@gmail.com>
2019-02-11 13:31:56 -0500

    loops through and adds background to even numbered rows

--- scripts.js
+++ scripts.js
@@ -4,8 +4,14 @@ for (let i = 0; i < myList.length; i += 1 ) {
   myList[i].style.color = 'purple';
 }
 
-var notPurple = document.getElementsByClassName('error-not-purple');
+var notPurple = document.querySelectorAll('.error-not-purple');
 
 for (let i = 0; i < notPurple.length; i += 1 ) {
   notPurple[i].style.color = 'red';
+}
+
+var evens = document.querySelectorAll('li:nth-child(even)');
+
+for (let i = 0; i < evens.length; i += 1 ) {
+  evens[i].style.backgroundColor = 'lightblue';
 }
