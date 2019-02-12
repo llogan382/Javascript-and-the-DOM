45f1617b70c9c6719dc7d6fefc74c55f8e25784c
Luke Logan <llogan382@gmail.com>
2019-02-12 06:00:36 -0500

    adds toggle button to show or hide a list

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
-let myHeading = document.querySelector('h1');
+const togglelist = document.getElementById('togglelist');
+const listDiv = document.querySelector('.list');
 
-const input = document.querySelector('input');
+const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.getElementsByClassName('description');
+
+togglelist.addEventListener('click', () => {
+  if (listDiv.style.display != "none") {
+    togglelist.textContent = "display list"
+    listDiv.style.display = "none"
+  } else {
+    togglelist.textContent = "hide list"
+    listDiv.style.display = "block";
+  }
+  
+})
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 })
+
+p.title = 'list description';
