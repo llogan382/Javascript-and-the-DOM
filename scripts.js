12ea8f643851cb33328a71ac32761388122447a4
Luke Logan <llogan382@gmail.com>
2019-02-12 06:48:26 -0500

    removes the last item in the list by clicking the button

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 const togglelist = document.getElementById('togglelist');
 const listDiv = document.querySelector('.list');
-
 const descriptionInput = document.querySelector('input.description');
 const descriptionP = document.querySelector('p.description');
-const descriptionButton = document.getElementsByClassName('description');
+const descriptionButton = document.querySelector('button.description');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const removeItemButton = document.querySelector('button.removeItemButton');
+
 
 
 togglelist.addEventListener('click', () => {
@@ -22,11 +23,16 @@ togglelist.addEventListener('click', () => {
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
   descriptionInput.value = '';
-})
+});
 addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
   ul.appendChild(li);
   addItemInput.value = '';
-})
+});
+removeItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
+  let li = document.querySelector('li:last-child');
+  ul.removeChild(li);
+});
