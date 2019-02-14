a2fcbfe006c5c36c58cc192da31e2d528407bfdb
Luke Logan <llogan382@gmail.com>
2019-02-14 06:47:08 -0500

    removes loop, targets LI items by TARGET property

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
-// listDiv.addEventListener('mouseover', () => {
-//     listItems[i].textContent = listItems[i].textContent.toUpperCase();
-// });
-// listDiv.addEventListener('mouseout', () => {
-//     listItems[i].textContent = listItems[i].textContent.toLowerCase();
-// });
 
-
-document.addEventListener('click', (event) => {
-    console.log(event.target);
+listDiv.addEventListener('mouseover', (event) => {
+    if (event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toUpperCase();
+    }
 });
+listDiv.addEventListener('mouseout', (event) => {
+    if (event.target.tagName == 'LI') {
+        event.target.textContent = event.target.textContent.toLowerCase();
+    }
+});
+
+
+
 
 
 togglelist.addEventListener('click', () => {
