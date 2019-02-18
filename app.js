e22556c667191356e82aa7e9bcd181e78983a6cd
Luke Logan <llogan382@gmail.com>
2019-02-18 06:28:07 -0500

    adds comments to code

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const addItemButton = document.querySelector('button.addItemButton');
 const listUL = document.querySelector('ul');
 const lis = listUL.children;
 
+//create a function that adds js using JS (not html)
 function attachListItemButtons(li){
   let up = document.createElement('button');
   up.className = "up";
   up.textContent = "up";
   li.appendChild(up);
+
   let down = document.createElement('button');
   down.className = "down";
   down.textContent = "down";
@@ -24,10 +26,12 @@ function attachListItemButtons(li){
   li.appendChild(remove);
 
 }
+//loop through HTML to add buttons using previous function and targeting children
 for(let i = 0; i < lis.length; i+=1){
   attachListItemButtons(lis[i]);
 }
 
+//object.method (object was devlared above). Adds event listener that targets clicks for each different button
 listUL.addEventListener('click', (event) => {
     if (event.target.tagName == 'BUTTON') {
       if(event.target.className == 'remove') {
@@ -59,8 +63,7 @@ listUL.addEventListener('click', (event) => {
   });
 
 
-
-
+// Hide list by changing display
 togglelist.addEventListener('click', () => {
   if (listDiv.style.display != "none") {
     togglelist.textContent = "display list"
@@ -72,10 +75,14 @@ togglelist.addEventListener('click', () => {
   
 })
 
+// Change title of liste by targeting innerhtml. Object.method, and objects are declared above
 descriptionButton.addEventListener('click', () => {
   descriptionP.innerHTML = descriptionInput.value + ':';
   descriptionInput.value = '';
 });
+
+// Add items with a click, and use the function created at the top of
+// this page (attachListItemButtons) to loop through and add buttons to page
 addItemButton.addEventListener('click', () => {
   let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
